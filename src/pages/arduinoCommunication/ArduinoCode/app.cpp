2b515c2fd4865e92c3a4b089d7425098b83be5fb
tsuanprang <tanakorn.suanprang@agoda.com>
2025-04-14 15:53:36 +0700

    Fix debug led

--- src/pages/arduinoCommunication/ArduinoCode/app.cpp
+++ src/pages/arduinoCommunication/ArduinoCode/app.cpp
@@ -113,8 +113,8 @@ void loop() {
     // Blink LEDs in the order specified by activeOrder
     for (int i = 0; i < 5; i++) {
       int pinIndex = activeOrder[i] - 1; // Convert 1-based index to 0-based index
-      Serial.println("DEBUG LED PIN: " + ledPins[pinIndex]);
       if (pinIndex >= 0 && pinIndex < 5) {
+        Serial.println("DEBUG LED PIN: " + String(ledPins[pinIndex]));
         digitalWrite(ledPins[pinIndex], HIGH); // Turn on the LED
         delay(ledDuration);                   // Wait for the duration
         digitalWrite(ledPins[pinIndex], LOW); // Turn off the LED
